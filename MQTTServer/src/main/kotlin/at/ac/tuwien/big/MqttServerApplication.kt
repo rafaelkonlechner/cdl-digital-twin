5b663bd051885c7de175993e97817fa6953ce887
Rafael Konlechner <rafael.konlechner@student.tuwien.ac.at>
2017-07-14 10:04:50 +0200

    Setup Spring Boot app with Gradle

---
+++ MQTTServer/src/main/kotlin/at/ac/tuwien/big/MqttServerApplication.kt
@@ -0,0 +1,13 @@
+package at.ac.tuwien.big
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+class MqttServerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MqttServerApplication::class.java, *args)
+}
