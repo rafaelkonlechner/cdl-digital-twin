0c28181a7e2913da85eea75b8e191c4149631956
Rafael Konlechner <rafael.konlechner@student.tuwien.ac.at>
2017-07-14 10:07:52 +0200

    Setup topics and subscriptions for MQTT and web sockets

---
+++ MQTTServer/src/main/kotlin/at/ac/tuwien/big/MessageController.kt
@@ -0,0 +1,88 @@
+package at.ac.tuwien.big
+
+import org.eclipse.paho.client.mqttv3.*
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.stereotype.Controller
+
+@Controller
+final class MessageController(val webSocket: SimpMessagingTemplate) : MqttCallback {
+
+    final val qos = 0
+    final val sensorTopic = "Sensor"
+    final val actuatorTopic = "Actuator"
+    final val detectionCameraTopic = "DetectionCamera"
+    final val pickupCameraTopic = "PickupCamera"
+    final val client: MqttClient
+
+    init {
+        println("Connecting to MQTT endpoint.")
+        client = MqttClient("tcp://localhost:1883", "Controller")
+        val connOpts = MqttConnectOptions()
+        connOpts.isCleanSession = true
+        client.connect(connOpts)
+        println("Established connection.")
+
+        println("Subscribing to topic: $sensorTopic")
+        client.setCallback(this)
+        client.subscribe(sensorTopic)
+        client.subscribe(detectionCameraTopic)
+        client.subscribe(pickupCameraTopic)
+    }
+
+    override fun messageArrived(topic: String?, message: MqttMessage?) {
+        when (topic) {
+            sensorTopic -> {
+                println("($topic) Received < ${String(message!!.payload)} >")
+                sendWebSocketMessageSensor(String(message.payload))
+            }
+            detectionCameraTopic -> {
+                println("($topic) Received < ${String(message!!.payload).substring(0, 20)} >")
+                sendWebSocketMessageDetectionCamera(String(message.payload))
+            }
+            pickupCameraTopic -> {
+                println("($topic) Received < ${String(message!!.payload).substring(0, 20)} >")
+                sendWebSocketMessagePickupCamera(String(message.payload))
+            }
+        }
+    }
+
+
+    override fun connectionLost(cause: Throwable?) {
+        throw cause ?: Exception("Connection lost.")
+    }
+
+    override fun deliveryComplete(token: IMqttDeliveryToken?) {
+        println("Delivery complete.")
+    }
+
+    fun sendMQTTMessageActuator(message: String) {
+        val tmp = MqttMessage(message.toByteArray())
+        tmp.qos = qos
+        println("Sending via MQTT: $message")
+        client.publish(actuatorTopic, tmp)
+    }
+
+    @MessageMapping("/actuator")
+    fun receiveActuatorWebSocketMessage(command: String) {
+        println("Actuator command: " + command)
+        sendMQTTMessageActuator(command)
+    }
+
+    private fun sendWebSocketMessageSensor(message: String) {
+        sendWebSocketMessage("/topic/sensor", message)
+    }
+
+    private fun sendWebSocketMessagePickupCamera(message: String) {
+        sendWebSocketMessage("/topic/pickupCamera", message)
+    }
+
+    private fun sendWebSocketMessageDetectionCamera(message: String) {
+        sendWebSocketMessage("/topic/detectionCamera", message)
+    }
+
+    private fun sendWebSocketMessage(topic: String, message: String) {
+        webSocket.send(topic, MessageBuilder.withPayload(message.toByteArray()).build())
+    }
+}
