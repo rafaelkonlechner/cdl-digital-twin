8530dd3439838ff4d1ea1b1e9977bc21935c6831
Rafael Konlechner <rafael.konlechner@student.tuwien.ac.at>
2017-09-14 09:51:24 +0200

    Clone one element for each disposed one

--- blender-scripts/modules/robot/greengate.py
+++ blender-scripts/modules/robot/greengate.py
@@ -1,4 +1,17 @@
 import GameLogic
+import bge
+import random
 
 def main():
     GameLogic.server.client.publish("Sensor", payload="{\"entity\": \"GreenGate\", \"event\": \"object-detected\"}", qos=0, retain=False)
+
+    GameLogic.greenCounter += 1
+    if (GameLogic.greenCounter % 2 == 1):
+        controller = bge.logic.getCurrentController()
+        owner = controller.owner
+        scene = bge.logic.getCurrentScene()
+        objects = scene.objectsInactive[1:]
+        choice = random.choice(objects)
+        hello = scene.addObject(choice, owner, 0)
+        hello.worldPosition = choice.worldPosition
+        hello.localScale = [1.0,1.0,1.0]
