3db1ec540481993589940013f8947c4a7f660a27
Rafael Konlechner <r.konlechner@gmail.com>
2018-04-30 13:11:41 +0200

    Resize tracked images to 200 x 200 for compatibility with previous tracking

--- ObjectTracking/index.js
+++ ObjectTracking/index.js
@@ -2,7 +2,7 @@ const tracking = require('./tracking-node')
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const png = require('png-js')
-const sizeOf = require('image-size')
+const jimp = require('jimp');
 const app = express()
 app.use(fileUpload())
 tracking.ColorTracker.registerColor('white', function(r, g, b) {
@@ -11,25 +11,30 @@ tracking.ColorTracker.registerColor('white', function(r, g, b) {
 var tracker = new tracking.ColorTracker(['white']);
 app.post('/analyze', function(req, res) {
     if (!req.files) return res.status(400).send('No image was found in the request.');
+    console.log("Analyzing image ...")
     let imageFile = req.files.file;
     imageFile.mv('current.png', function(err) {
         if (err) return res.status(500).send(err);
-        var dimensions = sizeOf('current.png');
-        png.decode('current.png', function(pixels) {
-            tracker.once('track', function(event) {
-                var rects = []
-                event.data.forEach(function(rect) {
-                    rects.push({
-                        x: rect.x,
-                        y: rect.y,
-                        width: rect.width,
-                        height: rect.height,
-                        color: rect.color
+        jimp.read('current.png').then(function(img) {
+            img.resize(200, 200).write('current1.png', function() {
+                png.decode('current1.png', function(pixels) {
+                    tracker.once('track', function(event) {
+                        var rects = []
+                        event.data.forEach(function(rect) {
+                            rects.push({
+                                x: rect.x,
+                                y: rect.y,
+                                width: rect.width,
+                                height: rect.height,
+                                color: rect.color
+                            });
+                        });
+                        console.log("Found " + rects.length + " items")
+                        res.send(rects)
                     });
+                    tracker.track(pixels, 200, 200)
                 });
-                res.send(rects)
             });
-            tracker.track(pixels, dimensions.width, dimensions.height)
         });
     });
 });
